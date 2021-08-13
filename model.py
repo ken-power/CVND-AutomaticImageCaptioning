b28b241113bc6e57bf6f120278aa748a4d11d601
Ken Power <ken.power@gmail.com>
2021-08-13 21:02:21 +0100

    Initial experiments with CNN Encoder and RNN Decoder

--- model.py
+++ model.py
@@ -5,29 +5,93 @@ import torchvision.models as models
 
 class EncoderCNN(nn.Module):
     def __init__(self, embed_size):
-        super(EncoderCNN, self).__init__()
+        # super(EncoderCNN, self).__init__()
+        super().__init__()
+
         resnet = models.resnet50(pretrained=True)
+
         for param in resnet.parameters():
             param.requires_grad_(False)
-        
+
         modules = list(resnet.children())[:-1]
         self.resnet = nn.Sequential(*modules)
         self.embed = nn.Linear(resnet.fc.in_features, embed_size)
+        self.init_weights()
 
     def forward(self, images):
         features = self.resnet(images)
         features = features.view(features.size(0), -1)
         features = self.embed(features)
+
         return features
-    
+
+    def init_weights(self):
+        """Initialize the weights."""
+        # self.linear.weight.data.normal_(0.0, 0.02)
+        # self.linear.bias.data.fill_(0)
+        pass
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
-    
+        # super(DecoderRNN, self).__init__()
+        super().__init__()
+
+        self.hidden_size = hidden_size
+
+        # embedding layer that turns words into a vector of a specified size
+        self.word_embeddings = nn.Embedding(vocab_size, embed_size)
+
+        # The LSTM takes embedded vectors as inputs
+        # and outputs hidden states of hidden_size
+        self.lstm = nn.LSTM(input_size=embed_size,
+                            hidden_size=hidden_size,
+                            num_layers=num_layers,
+                            batch_first=True)
+
+        # the linear layer that maps the hidden state output dimension
+        self.linear = nn.Linear(hidden_size, vocab_size)
+        self.init_weights()
+
     def forward(self, features, captions):
+        captions = captions[:, :-1]
+        embeds = self.word_embeddings(captions)
+
+        # Concatenating features to embedding
+        # torch.cat 3D tensors
+        inputs = torch.cat((features.unsqueeze(1), embeds), 1)
+
+        lstm_out, hidden = self.lstm(inputs)
+        outputs = self.linear(lstm_out)
+
+        return outputs
+
+    def init_weights(self):
+        """Initialize weights."""
+
+        # self.embed.weight.data.uniform_(-0.1, 0.1)
+        # self.linear.weight.data.uniform_(-0.1, 0.1)
+        # self.linear.bias.data.fill_(0)
         pass
 
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
-        pass
+        predicted_sentence = []
+
+        for i in range(max_len):
+            lstm_out, states = self.lstm(inputs, states)
+
+            lstm_out = lstm_out.squeeze(1)
+            lstm_out = lstm_out.squeeze(1)
+            outputs = self.linear(lstm_out)
+
+            # Get maximum probabilities
+            target = outputs.max(1)[1]
+
+            # Append result into predicted_sentence list
+            predicted_sentence.append(target.item())
+
+            # Update the input for next iteration
+            inputs = self.word_embeddings(target).unsqueeze(1)
+
+        return predicted_sentence
+
