4082bd32f9dd0749ba235e1cd49bbfd23b3c0af3
Ken Power <ken.power@gmail.com>
2021-08-14 14:51:09 +0100

    Refactor: remove unused code, tidy up comments

--- model.py
+++ model.py
@@ -5,7 +5,6 @@ import torchvision.models as models
 
 class EncoderCNN(nn.Module):
     def __init__(self, embed_size):
-        # super(EncoderCNN, self).__init__()
         super().__init__()
 
         resnet = models.resnet50(pretrained=True)
@@ -16,7 +15,6 @@ class EncoderCNN(nn.Module):
         modules = list(resnet.children())[:-1]
         self.resnet = nn.Sequential(*modules)
         self.embed = nn.Linear(resnet.fc.in_features, embed_size)
-        self.init_weights()
 
     def forward(self, images):
         features = self.resnet(images)
@@ -25,15 +23,9 @@ class EncoderCNN(nn.Module):
 
         return features
 
-    def init_weights(self):
-        """Initialize the weights."""
-        # self.linear.weight.data.normal_(0.0, 0.02)
-        # self.linear.bias.data.fill_(0)
-        pass
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        # super(DecoderRNN, self).__init__()
         super().__init__()
 
         self.hidden_size = hidden_size
@@ -41,8 +33,7 @@ class DecoderRNN(nn.Module):
         # embedding layer that turns words into a vector of a specified size
         self.word_embeddings = nn.Embedding(vocab_size, embed_size)
 
-        # The LSTM takes embedded vectors as inputs
-        # and outputs hidden states of hidden_size
+        # The LSTM takes embedded vectors as inputs and outputs hidden states of hidden_size
         self.lstm = nn.LSTM(input_size=embed_size,
                             hidden_size=hidden_size,
                             num_layers=num_layers,
@@ -50,7 +41,6 @@ class DecoderRNN(nn.Module):
 
         # the linear layer that maps the hidden state output dimension
         self.linear = nn.Linear(hidden_size, vocab_size)
-        self.init_weights()
 
     def forward(self, features, captions):
         captions = captions[:, :-1]
@@ -65,16 +55,13 @@ class DecoderRNN(nn.Module):
 
         return outputs
 
-    def init_weights(self):
-        """Initialize weights."""
-
-        # self.embed.weight.data.uniform_(-0.1, 0.1)
-        # self.linear.weight.data.uniform_(-0.1, 0.1)
-        # self.linear.bias.data.fill_(0)
-        pass
-
     def sample(self, inputs, states=None, max_len=20):
-        " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
+        """
+        Accepts pre-processed image tensor (inputs) and returns predicted sentence
+
+        :param inputs: pre-processed image tensor
+        :return: predicted sentence (list of tensor ids of length max_len)
+        """
         predicted_sentence = []
 
         for i in range(max_len):
@@ -94,4 +81,3 @@ class DecoderRNN(nn.Module):
             inputs = self.word_embeddings(target).unsqueeze(1)
 
         return predicted_sentence
-
